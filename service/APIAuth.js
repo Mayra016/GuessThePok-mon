a73af4839286d2095c7a276f7efae3204dbc192e
Mayra016 <mayrazucaro@gmail.com>
2025-02-05 14:03:17 -0300

    feat add api response image card to website view and blur effect

--- service/APIAuth.js
+++ service/APIAuth.js
@@ -1,15 +1,18 @@
 import config from '../config/config.js';
-export default function getImage() {
-    fetch(config.API_URL + "/hgss4-1", {
-        method: 'GET',
-        headers: {
-          'Authorization': `Bearer ${config.SECRET_KEY}`
-        }
-      })
-      .then(response => response.json())
-      .then(data => {
-        console.log(data.data.images.large);
-      })
-      .catch(error => console.error('Error:', error));
-      
+
+export default async function getImage() {
+    try {
+        const response = await fetch(config.API_URL + "/hgss4-1", {
+            method: 'GET',
+            headers: {
+                'Authorization': `Bearer ${config.SECRET_KEY}`
+            }
+        });
+        const data = await response.json();
+        console.log("Fetched data:", data); 
+        return data.data.images.small; 
+    } catch (error) {
+        console.error('Error:', error);
+        return null;
+    }
 }
