bbc340a70ccc5c50ad339c10626ddbfa7a471af0
Mayra016 <mayrazucaro@gmail.com>
2025-02-06 11:28:45 -0300

    feat translations added in game.html

--- config/translationsEN.js
+++ config/translationsEN.js
@@ -1,7 +1,13 @@
 const textsEN = {
     titleIndex : 'How to play?',
     helpTextIndex : "Try to guess the pokémon displayed in the blurred card \n \u25E6 For each try, the blur will be a little vanished \n \u25E6 You will lose if you can't guess the pokémon in 20 tries in the same card",
-    playIndex : 'Play'
+    playIndex : 'Play',
+    menuBtn : 'Menu',
+    titleGame : "Who's that Pokémon?",
+    searchBarPlaceholder : "Enter the Pokémon's name",
+    sendBtn: 'Send',
+    scoreText : 'Your score was ',
+    playAgain : 'Jugar otra'
 };
 
 export default textsEN;
--- config/translationsES.js
+++ config/translationsES.js
@@ -1,7 +1,13 @@
 const textsES = {
     titleIndex : '¿Cómo jugar?',
     helpTextIndex : 'Intenta adivinar el pokémon en la carta borrosa \n \u25E6 A cada intento el efecto borroso se va a reducir un poco \n \u25E6 Pierdes si no adivinas el pokémon en 20 intentos en la misma carta',
-    playIndex : 'Jugar'
+    playIndex : 'Jugar',
+    menuBtn: 'Menú',
+    titleGame : '¿Quién es este Pokémon?',
+    searchBarPlaceholder : 'Inserta el nombre del Pokémon',
+    sendBtn : 'Enviar',
+    scoreText : 'Tu puntuación fue ',
+    playAgain : 'Jugar otra'
 };
 
 export default textsES;
