695885a3b05d965c43bdb6eaccb0da3eeb614b0d
Mayra016 <mayrazucaro@gmail.com>
2025-02-06 10:52:45 -0300

    feat translation improvement with config files

---
+++ config/translationsEN.js
@@ -0,0 +1,7 @@
+const textsEN = {
+    titleIndex : 'How to play?',
+    helpTextIndex : "Try to guess the pokémon displayed in the blurred card \n \u25E6 For each try, the blur will be a little vanished \n \u25E6 You will lose if you can't guess the pokémon in 20 tries in the same card",
+    playIndex : 'Play'
+};
+
+export default textsEN;
---
+++ config/translationsES.js
@@ -0,0 +1,8 @@
+const textsES = {
+    titleIndex : '¿Cómo jugar?',
+    helpTextIndex : 'Intenta adivinar el pokémon en la carta borrosa \n \u25E6 A cada intento el efecto borroso se va a reducir un poco \n \u25E6 Pierdes si no adivinas el pokémon en 20 intentos en la misma carta',
+    playIndex : 'Jugar'
+};
+
+export default textsES;
+
